4d292a219ac922bc34c7f7a1008314fe44ca5320
Felipe Muñoz Orellana <felipe.munoz.o@hotmail.com>
2022-02-18 23:57:43 -0300

    solution to Walmart challenge clean code

--- README_FM.md
+++ README_FM.md
@@ -5,3 +5,9 @@ Youn can find my architecture in the "architecture case" folder.
 # ETL Job
 
 To solve this problem I take a developer approach, I work every day with streaming and batch pipelines in the GCP ecosystem, but a choose to create a class that can be use by any ingestion tool that includes python as a language.
+
+To execute this script, you can find the external packages in the requirements.txt file, and installing this packages using pip
+
+Example:
+pip install pandas
+
--- src/classes/etl.py
+++ src/classes/etl.py
@@ -12,7 +12,9 @@ class ETL:
     '''
     def __init__(self):
         print("loading...")
-
+    '''
+    Function that performs extraction from DATA folder 
+    '''
     def extract(folder):
         df = pd.DataFrame()
         path = Path(dirname(abspath(__file__)))
@@ -22,20 +24,16 @@ class ETL:
         for file in files:
             f = open(str(path)+"\\data\\"+file)
             data = json.load(f)
-            #print(data)
             df_temp = pd.json_normalize(data)
             df_temp['season'] = file.replace('_json.json','')
-            #print(df_temp)
             df_temp = pd.concat([df_temp,pd.get_dummies(df_temp['FTR'])], axis=1)
-            #print(df_temp)
             frames = [df, df_temp]
             df = pd.concat(frames)
-            #df = df.append(df_temp,ignore_index=True)
 
-        #print(df)
-        print("extracting...")
         return df
-
+    '''
+    Function that performs transformation of data, creating position table 
+    '''
     def position_table_transform(df_results):
 
         position_table = df_results[['season','HomeTeam','AwayTeam','FTHG','FTAG', 'H', 'D','A', 'HY', 'HR','AY','AR']]
@@ -54,27 +52,33 @@ class ETL:
         #pd.set_option("display.max_rows", None, "display.max_columns", None)
         
 
-        print("transforming...")
         return seasons_position_table
-
+    '''
+    Function that performs transformation of data, listing the dirtiest team for every season
+    '''
     def dirtiest_team_transform(df_results):
         df_dirtiest = df_results.groupby(['season'], sort=False)['RedCards','YellowCards'].max()
-        print("transforming...")
+        dff = pd.merge(df_results,df_dirtiest,on=['season','RedCards','YellowCards'])
+        print(dff)
         #idx = df_results.index[df_dirtiest['RedCards'] == df_results['RedCards']]
         #print(idx)
         #return df_results[idx]#
         return df_results.groupby(['season'], sort=False)['RedCards','YellowCards'].max()
         #.sort_values(by=['season','Points'], ascending=(True,False))
 
+    '''
+    Function that performs transformation of data, listing the top socring teams by season 
+    '''
     def top_scoring_team_transform(df_results):
         idx = df_results.groupby(['season'])['Goals'].transform(max) == df_results['Goals']
-        print("transforming...")
         return df_results[idx]
     
+    '''
+    Function that performs load of data, creating csv file for a dataframe, providing the name of the file 
+    '''
     def load_data(df_results, name):
         path = Path(dirname(abspath(__file__)))
         path = path.parent.absolute()
         df_results.to_csv('{0}/output/{1}.csv'.format(str(path),name)) 
-        print("loaded")
 
 
